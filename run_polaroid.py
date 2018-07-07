a7d918087d3d2625152b0cb1b07ac6962dd5bd6e
Kevin Ting <kmting@gmail.com>
2018-07-07 01:28:55 -0700

    it finally workssssslsls but needs some tweaks to optimize internet settings

--- run_polaroid.py
+++ run_polaroid.py
@@ -1,11 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 from subprocess import call
+from Adafruit_Thermal import *
+from PIL import Image
+import pdb
 
+#CONSTANTS
 TRIGGER_PIN = 4
-GPIO.setmode(GPIO.BCM)
+PICTURE_PATH = "TEST.BMP"
+PRINTER_WIDTH = 384
+PRINTER_HEIGHT = 384
 
+#SETUP
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(TRIGGER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
+
+
 
 while True:
     input_state = GPIO.input(TRIGGER_PIN)
@@ -13,9 +25,20 @@ while True:
         print('Button pressed')
         
         print "capturing image"
-        call(["raspistill","-v","-n","-o","test.bmp"])
+        call(["raspistill","-v","-n","-o",PICTURE_PATH])
         
         print "printing image"
-        call(["lp","-o","fit-to-page","-c","test.bmp"])
+        im = Image.open(PICTURE_PATH)
+        scaled = im.resize((PRINTER_WIDTH, PRINTER_HEIGHT))
         
+        #call(["lp","-o","fit-to-page","-c","test.bmp"])
+        printer.printImage(scaled, LaaT=True)
+        
+        pdb.set_trace()
+        
+        #add blank prints so image is fully out of the printer
+        printer.println("")
+        printer.println("")
+        printer.println("")
+
         time.sleep(0.2)
